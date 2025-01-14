6c5bcbdfa0c0ae137d3ecc3b17a119c054786bcd
nigbird <birdnig00@gmail.com>
2025-01-14 11:54:37 +0300

    R3 deployment fix

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
+
+# Expose the WSGI callable as a module-level variable named `app`
+app = application
