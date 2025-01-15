bb26b4e6d9fa4dd1d9ac1c520e0b73f3bbe37efe
nigbird <birdnig00@gmail.com>
2025-01-15 10:06:57 +0300

    css R trrial

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your-secret-key'
+SECRET_KEY = '1212'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
