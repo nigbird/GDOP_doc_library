5ea5a153c8329d831acc5a7601922e442194759d
nigbird <birdnig00@gmail.com>
2025-01-15 10:43:29 +0300

    db R2 trrial

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,10 +77,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'postgres://user:password@hostname:port/database')
+    )
 }
 
 
